ffb3512856bed3fbf7380f2d7b97ab3cc6d71ecf
Ashraf Jaman <ashrafjaman247@gmail.com>
2022-02-28 15:51:45 +0600

    Add Server

--- api/routes/students.js
+++ api/routes/students.js
@@ -1,23 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const student = require('../models/student');
+
+const Student = require('../models/student');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message:"Get Request"
-    })
+    student.find()
+    .exec()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err));
 })
 
-router.post('/',(req, res, next) => {
-    res.status(201).json({
-        message:"Post Request"
-    })
+router.post('/', (req, res, next) => {
+    const info = {
+        _id: mongoose.Types.ObjectId(),
+        name: req.body.name,
+        batch: req.body.batch
+    }
+
+    const student = new Student(info);
+
+    student.save()
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(500).json(err));
 })
 
+router.get('/:studentId', (req, res, next) => {
 
+    const id = req.params.studentId;
+    student.findById(id)
+    .exec()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err));
+})
 
+router.delete('/:studentId', (req, res, next) => {
 
+    const id = req.params.studentId;
+    student.deleteOne({_id: id})
+    .exec()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json(err));
+})
 
+router.patch('/:studentId', (req, res, next) => {
+    const id = req.params.studentId;
 
+    const updateOps = {};
+    for(const info of req.body){
+        updateOps[info.key] = info.value;
+    }
 
+Student.updateOne({_id: id},{$set: updateOps})
+.exec()
+.then(result => res.status(200).json(result))
+.catch(err => res.status(500).json({
+    message: "Error"
+}));
+})
 
 module.exports = router;
